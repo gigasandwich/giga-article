a28d2df15e3ea899d88ba0983913b4adb5439168
Yvan Noah <yvannandy@gmail.com>
2026-03-30 20:09:46 +0300

    Integration article details
    Co-Authored-By: Harinandrasana Steeve RAJAONERA <192488439+HarySteeve@users.noreply.github.com>

--- front/frontoffice/article-details.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../../back/model/Article.php";
-require_once "../../back/db/Connection.php";
-
-$article_id = $_GET["article_id"];
-$article = Article::getById($pdo, $article_id);
-
-print_r($article);
