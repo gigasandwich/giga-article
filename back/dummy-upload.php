83aa43f8375673add09471fbdb2704ca0927ec72
Yvan Noah <yvannandy@gmail.com>
2026-03-30 14:07:09 +0300

    Remove unecessary files

--- back/dummy-upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Take a look at the image xD
-echo json_encode(
-    [
-        "location" => "https://imgs.search.brave.com/3xHKdBoFqpJd2lw8anG0_TsgdYISaIWZlTGwEQHgtg0/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/Ym9yZWRwYW5kYS5j/b20vYmxvZy93cC1j/b250ZW50L3VwbG9h/ZHMvMjAyMS8wOS9m/Yl9pbWFnZV82MTM3/MzQ4OWVlMjU1LXBu/Z19fNzAwLmpwZz91/dG1fY2FtcGFpZ249/cmViZWxib29zdF90/cnVl"
-    ]
-);
